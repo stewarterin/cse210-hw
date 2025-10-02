b6c0526311c6b38f6b139e71670b526a5d35eaba
stewarterin <erinlu@live.com>
2025-10-02 16:36:07 -0600

    Learning02 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "IBM";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2003";
+        job1._endYear = "2010";
+
+        Job job2 = new Job();
+
+        job2._company = "Adobe";
+        job2._jobTitle = "Web Developer";
+        job2._startYear = "2010";
+        job2._endYear = "2025";
+
+        Resume myResume = new Resume();
+        myResume._name = "Sarah Ranjel";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
