1cd568cbc8a0afe770283266cb7a728aae682fbb
stewarterin <erinlu@live.com>
2025-10-09 02:01:16 -0600

    journal program.cs code- display menu and if/else layout

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int userInput = -1;
+        while (userInput != 5)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please sselect one of the following choices: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == 1)
+            {
+
+            }
+            else if (userInput == 2)
+            {
+
+            }
+            else if (userInput == 3)
+            {
+
+            }
+            else if (userInput == 4)
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
     }
 }
