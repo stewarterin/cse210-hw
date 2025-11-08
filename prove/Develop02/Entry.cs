f23f6e6506b68306bff3b5d64b24476093101933
stewarterin <erinlu@live.com>
2025-11-07 22:46:12 -0700

    Added "using" lines at the top

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public class Entry
 {
     public string _date;
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
     public List<string> _prompts = new List<string>
