b22d9141efe28fb5c3fc4946b2e6570a7b64b684
stewarterin <erinlu@live.com>
2025-12-02 14:25:34 -0700

    working on Word constructor and behaviors

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -3,24 +3,37 @@ public class Word
     private string _word;
 
     private bool _hidden; 
+
+    public Word(string word)
+    {
+        _word = word;
+        _hidden = ;
+    }
     
     public void Hide()
     {
-        
+        _hidden = true; 
     }
 
-    // public string Show()
-    // {
-    //     return ;
-    // }
+    public string Show()
+    {
+        if (_hidden == true)
+        {
+            return ("_____");
+        }
+        else
+        {
+            return _word;
+        }
+    }
 
-    // public bool IsHidden()
-    // {
-    //     return ;
-    // }
+    public bool IsHidden()
+    {
+        return ;
+    }
 
-    // public string GetWord()
-    // {
-    //     return ;
-    // }
+    public string GetWord()
+    {
+        return ;
+    }
 }
