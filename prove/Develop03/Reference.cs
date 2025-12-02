1872b641d5565e28aefe284b7817a344defb29f1
stewarterin <erinlu@live.com>
2025-12-02 01:08:45 -0700

    Starting develop 3 setup

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,29 @@
+using System.Dynamic;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, string chapter, string verse)
+    {
+        _book =  ;
+        _chapter = ;
+        _verse = ; 
+    }
+
+    public Reference(string book, string chapter, string verse, string endVerse)
+    {
+        _book =  ;
+        _chapter = ;
+        _verse = ; 
+        _endVerse = ;
+    }
+
+    public string GetReference()
+    {
+        return ; 
+    } 
+}
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,5 @@
+public class Scripture
+{
+    private string _reference;
+    private List<Word> _words; 
+}
