0c7469aebe7e6ea35de465d3d631ed234da308b7
stewarterin <erinlu@live.com>
2025-12-08 02:03:33 -0700

    Working on Scripture constructor and methods

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,9 +3,29 @@ public class Scripture
     private string _reference;
     private List<Word> _words; 
 
+    public Scripture(string reference, string scripture)
+    {
+        _reference = reference;
+
+        string[] splitScripture = scripture.Split(' ');
+        _words = new List<Word>();
+        foreach (string word in splitScripture)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
     public void HideWords()
     {
-        
+        List<Word> showWords = new List<Word>();
+
+        foreach(Word word in _words)
+        {
+            if (word.IsHidden() == false)
+            {
+                showWords.Add(word);
+            }
+        }
     }
 
     public void GetText()
@@ -13,13 +33,24 @@ public class Scripture
         
     } 
 
-    public bool IsCompletelyHidden()
+    public bool IsCompletelyHidden() //Problem here. Method needs to return bool outside of the if/else loop..
     {
+        foreach (Word word in _words)
+        {
+            if (word.IsHidden() == false)
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+        }
         
     }
 
-    public string DisplayText()
-    {
+    // public string DisplayText()
+    // {
         
-    }
+    // }
 }
