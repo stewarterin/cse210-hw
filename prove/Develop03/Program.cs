49853cb5692bd487ca5213c242e1f9d535d630cf
stewarterin <erinlu@live.com>
2025-12-08 02:04:20 -0700

    Clear console and writeline code added

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,12 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Reference r1 = new Reference("John", 3, 16);
-        Console.WriteLine(r1.GetReference());
+        // Console.WriteLine(r1.GetReference());
 
-        Reference r2 = new Reference("John", 3, 16, 17);
-        Console.WriteLine(r2.GetReference());
+        // Reference r2 = new Reference("John", 3, 16, 17);
+        // Console.WriteLine(r2.GetReference());
 
         Word w1 = new Word("HELLO");
-        Console.WriteLine(w1.GetWord());
+        // Console.WriteLine(w1.GetWord());
+
+        //Scripture s1 = new Scripture(reference);
+
+        Console.WriteLine("SCRIPTURE HERE");
+        Console.WriteLine("Press enter to continue or type 'quit' to finisih:");
+        Console.ReadLine();
+
+        Console.Clear();
+
+        Console.WriteLine("ADJUSTED SCRIPTURE HERE");
     }
 }
