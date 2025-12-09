9c94643d61e034c50bb8925784d30bab3dece706
stewarterin <erinlu@live.com>
2025-12-09 02:14:08 -0700

    Outline for ReflectingActivity class

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,47 @@
+public class ReflectingActivity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "",
+        "",
+        ""        
+    };
+    private List<string> _questions = new List<string>
+    {
+        "",
+        "",
+        ""       
+    };
+
+
+
+    public ReflectingActivity()
+    {
+        
+    }
+
+    public void RunReflecting()
+    {
+        
+    }
+
+    public string GetRandomPrompt()
+    {
+        return ;
+    }
+
+    public string GetRandomQuestion()
+    {
+        return ;
+    }
+
+    public void DisplayPrompt()
+    {
+        
+    }
+
+        public void DisplayQuestion()
+    {
+        
+    }
+}
