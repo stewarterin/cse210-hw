dd1ab0b3df5c2d95b5bcea156f959f3f11f3a926
stewarterin <erinlu@live.com>
2025-12-09 02:13:53 -0700

    Outline for ListingActivity class

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,29 @@
+public class ListingActivity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "",
+        "",
+        ""
+    };
+
+    public ListingActivity()
+    {
+        
+    }
+
+    public void RunListing()
+    {
+        
+    }
+
+    public void GetRandomPrompt()
+    {
+        
+    }
+
+    public List<string> GetUserList()
+    {
+        return ;
+    }
+}
