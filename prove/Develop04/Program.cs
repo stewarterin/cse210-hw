345d6d453aeb49b0a0121a07f946c3c703791f2d
stewarterin <erinlu@live.com>
2025-12-09 01:48:33 -0700

    Outline for Program class

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int userInput = -1;
+
+        while (userInput != 4)
+        {
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+        Console.Write("Select a choice from the menu: ");
+
+        userInput = int.Parse(Console.ReadLine());
+
+        if (userInput == 1)
+            {
+                
+            }
+        else if (userInput == 2)
+            {
+                
+            }
+        else if (userInput == 3)
+            {
+                
+            }
+        else
+            {
+                Environment.Exit(0);
+            }
+        }
     }
 }
