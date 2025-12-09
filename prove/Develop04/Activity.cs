e505e5f8f5f19376ff4afe4a031d427d2156e4b8
stewarterin <erinlu@live.com>
2025-12-09 01:49:10 -0700

    Outline for Activity class

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,31 @@
+public class Activity
+{
+    private string _activityName;
+    private string _description;
+    private int _durationSeconds;
+
+    public Activity()
+    {
+        
+    }
+
+    public void DisplayStartMessage()
+    {
+        
+    }
+
+    public void DisplayEndMessage()
+    {
+        
+    }
+
+    public void DisplaySpinner()
+    {
+        
+    }
+
+    public void DisplayCountdown()
+    {
+        
+    }
+}
