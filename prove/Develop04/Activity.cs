d321a8c59b1c24e8347e0c9668619486e58a0edf
stewarterin <erinlu@live.com>
2025-12-10 03:18:20 -0700

    Added Activity constructor, Display messsages, and DateTime to DisplayCOuntdown

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -4,19 +4,26 @@ public class Activity
     private string _description;
     private int _durationSeconds;
 
-    public Activity()
+    public Activity(string name, string description, int seconds)
     {
-        
+        _activityName = name;
+        _description = description;
+        _durationSeconds = seconds;
     }
 
     public void DisplayStartMessage()
     {
-        
+        Console.WriteLine($"Welcome to the {_activityName}.");
+        Console.WriteLine($"\n{_description}");
+        Console.WriteLine("How long, in seconds, would you like for your session?");
+        _durationSeconds = int.Parse(Console.ReadLine());
     }
 
     public void DisplayEndMessage()
     {
-        
+        Console.WriteLine($"Well done!!");
+        DisplaySpinner();
+        Console.WriteLine($"You have completed another {_durationSeconds} seconds of the {_activityName}.");
     }
 
     public void DisplaySpinner()
@@ -34,15 +41,15 @@ public class Activity
     public void DisplayCountdown(int seconds)
     {
         _durationSeconds = seconds;
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
         
-        while (_durationSeconds > 0)
+        while (DateTime.Now < endTime)
         {
             Console.Write($"{_durationSeconds}");
             Console.Write("\b");
             Thread.Sleep(1000);
             _durationSeconds--;
         }
-
-        Console.WriteLine("\b");  
     }
 }
