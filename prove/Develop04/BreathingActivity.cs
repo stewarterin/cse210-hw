76970b026c3f8bd9ceec7d583cc765955712d89b
stewarterin <erinlu@live.com>
2025-12-11 01:07:08 -0700

    Added inheritance, constructor code, method code

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,12 +1,21 @@
-public class BreathingActivity
+public class BreathingActivity : Activity
 {
     public BreathingActivity()
     {
-        
+        _activityName = "Breathing Activity";
+        _description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
     }
 
     public void RunBreathing()
     {
-        
+        DisplayStartMessage();
+
+        Console.WriteLine("Get Ready...");
+        DisplaySpinner();
+        Console.Write("\nBreathe in...");
+        DisplayCountdown(4);
+        Console.Write("Now breathe out...");
+        DisplayCountdown(4);
+
     }
 }
