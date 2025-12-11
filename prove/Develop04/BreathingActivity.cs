47a3b20acca04280c2139ff87154b6b081020d4e
stewarterin <erinlu@live.com>
2025-12-11 02:57:14 -0700

    Added call to base in constructor, fixed code for method, moved datetime to the method

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,21 +1,29 @@
 public class BreathingActivity : Activity
 {
-    public BreathingActivity()
-    {
-        _activityName = "Breathing Activity";
-        _description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
-    }
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
+    {}
 
     public void RunBreathing()
     {
-        DisplayStartMessage();
+        Console.Clear();
 
+        DisplayStartMessage();
+        
         Console.WriteLine("Get Ready...");
         DisplaySpinner();
-        Console.Write("\nBreathe in...");
-        DisplayCountdown(4);
-        Console.Write("Now breathe out...");
-        DisplayCountdown(4);
 
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_durationSeconds);
+        
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("\nBreathe in...");
+            DisplayCountdown(5);
+            Console.Write("\nNow breathe out...");
+            DisplayCountdown(5);
+            Console.WriteLine("");
+        }
+
+        DisplayEndMessage();
     }
 }
