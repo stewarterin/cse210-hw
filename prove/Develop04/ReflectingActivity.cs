1941c2e0ba6c9ee5b9daada0cff87c867f407c5b
stewarterin <erinlu@live.com>
2025-12-12 02:13:31 -0700

    Added content to lists, added GetRandom and Display methods, fixed Run code to display right

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -2,15 +2,22 @@ public class ReflectingActivity : Activity
 {
     private List<string> _prompts = new List<string>
     {
-        "",
-        "",
-        ""        
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless"        
     };
     private List<string> _questions = new List<string>
     {
-        "",
-        "",
-        ""       
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"     
     };
 
     public ReflectingActivity() : base("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
@@ -23,11 +30,10 @@ public class ReflectingActivity : Activity
         DisplayStartMessage();
         
         Console.WriteLine("Get Ready...");
-        DisplaySpinner();
-        Console.WriteLine("Consider the following prompt:");
+        DisplaySpinner(7);
+        Console.WriteLine("\nConsider the following prompt:");
         
-        string prompt = _prompts.GetRandomPrompt();
-        Console.WriteLine($"--- {prompt} ---");
+        DisplayPrompt();
         Console.WriteLine("When you have something in mind, press enter to continue.");
         Console.ReadLine();
         Console.WriteLine("Now ponder on each of the following questions as they realted to this experience.");
@@ -41,9 +47,8 @@ public class ReflectingActivity : Activity
         
         while (DateTime.Now < endTime)
         {
-            string question = _questions.GetRandomQuestion();
-            Console.WriteLine($"> {question} ");
-            DisplaySpinner(10);
+            DisplayQuestion();
+            DisplaySpinner(15);
         }
 
         DisplayEndMessage();
@@ -51,21 +56,29 @@ public class ReflectingActivity : Activity
 
     public string GetRandomPrompt()
     {
-        // return ;
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        string _prompt = _prompts[index];
+        return _prompt;
     }
 
     public string GetRandomQuestion()
     {
-        // return ;
+        Random random = new Random();
+        int index = random.Next(_questions.Count);
+        string _question = _questions[index];
+        return _question;
     }
 
     public void DisplayPrompt()
     {
-        
+        string _prompt = GetRandomPrompt();
+        Console.WriteLine($" --- {_prompt} --- ");
     }
 
     public void DisplayQuestion()
     {
-        
+        string _question = GetRandomQuestion();
+        Console.Write($"> {_question} ");    
     }
 }
