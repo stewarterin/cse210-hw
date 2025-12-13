7c8ed2a21dda0dba2331314faeda7d44ec50aaaf
stewarterin <erinlu@live.com>
2025-12-13 01:40:12 -0700

    Added list contents, fixed constructor to match other classes, Added code for all methods

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -2,29 +2,64 @@ public class ListingActivity : Activity
 {
     private List<string> _prompts = new List<string>
     {
-        "",
-        "",
-        ""
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
     };
 
-    public ListingActivity()
-    {
-        _activityName = "Listing Activity";
-        _description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
-    }
+    public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {}
 
     public void RunListing()
     {
+        Console.Clear();
+
+        DisplayStartMessage();
         
+        Console.WriteLine("Get Ready...");
+        DisplaySpinner(7);
+
+        Console.WriteLine("\nList as many responses as you can to the following prompt:");
+        DisplayPrompt();
+        Console.WriteLine("You may begin in: ");
+        DisplayCountdown(5);
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_durationSeconds);
+        
+        List<string> userList = GetUserList(endTime);
+
+        Console.WriteLine($"You listed {userList.Count} items!");
+
+        DisplayEndMessage();
     }
 
-    public void GetRandomPrompt()
+    public string GetRandomPrompt()
     {
-        
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        string _prompt = _prompts[index];
+        return _prompt;       
     }
 
-    // public List<string> GetUserList()
-    // {
-    //     return ;
-    // }
+    public void DisplayPrompt()
+    {
+        string _prompt = GetRandomPrompt();
+        Console.WriteLine($" --- {_prompt} --- ");
+    }
+
+    public List<string> GetUserList(DateTime endTime)
+    {
+        List<string> responses = new List<string>();
+        
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string userEntry = Console.ReadLine();
+            responses.Add(userEntry);
+        }
+        return responses; 
+    }
 }
