62484dbb3c71271a83cb1be5ee8e2b038b47315d
stewarterin <erinlu@live.com>
2025-12-13 23:18:32 -0700

    Learning05 assignment completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Square s1 = new Square("blue", 2);
+        // Console.WriteLine(s1.GetColor());
+        // Console.WriteLine(s1.GetArea());
+
+        // Rectangle r1 = new Rectangle("green", 4, 2);
+        // Console.WriteLine(r1.GetColor());
+        // Console.WriteLine(r1.GetArea());
+
+        // Circle c1 = new Circle("purple", 2);
+        // Console.WriteLine(c1.GetColor());
+        // Console.WriteLine(c1.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 2);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("green", 4, 2);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("purple", 2);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Area: {area}");
+        }
     }
 }
