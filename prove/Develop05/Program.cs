985dd2639ec0d967d9bbb6b20a802c6d39dafbd9
stewarterin <erinlu@live.com>
2025-12-15 02:38:49 -0700

    Added lines to divide displaycontent, commented out test code, added code for userInput 1

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -8,6 +8,8 @@ class Program
 
         while (userInput !=6)
         {
+            Console.WriteLine("───────────────────────────────");
+            Console.WriteLine("Welcome to the Eternal Quest Program!");
             Console.WriteLine("Menu Options:");
             Console.WriteLine("1. Create New Goal");
             Console.WriteLine("2. List Goals");
@@ -18,48 +20,46 @@ class Program
             Console.Write("Select a choice from the menu: ");
 
             userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("───────────────────────────────");
+
+            
 
             if (userInput == 1)
             {
-                Console.WriteLine("The types of goals are:");
-                Console.WriteLine("1. Simple Goal");
-                Console.WriteLine("2. Eternal Goal");
-                Console.WriteLine("3. Checklist Goal");
-                Console.Write("Which type of goal would you like to create? "); 
-
-                               
+                GoalTracker g1 = new GoalTracker();
+                g1.CreateNewGoal();          
             }
             else if (userInput == 2)
             {
-                SimpleGoal s1 = new SimpleGoal("SimpleT", "SimpleT", 10, true);
-                s1.GetGoal();
-                s1.RecordEvent();
-                Console.WriteLine(s1.IsComplete());
-                Console.WriteLine("____________________________");
+                // SimpleGoal s1 = new SimpleGoal("SimpleT", "SimpleT", 10, true);
+                // s1.GetGoal();
+                // s1.RecordEvent();
+                // Console.WriteLine(s1.IsComplete());
+                // Console.WriteLine("____________________________");
 
-                SimpleGoal s2 = new SimpleGoal("Simple2", "Simple2", 20, false);
-                s2.GetGoal();
-                s2.RecordEvent();
-                Console.WriteLine(s2.IsComplete());
-                Console.WriteLine("____________________________");
+                // SimpleGoal s2 = new SimpleGoal("Simple2", "Simple2", 20, false);
+                // s2.GetGoal();
+                // s2.RecordEvent();
+                // Console.WriteLine(s2.IsComplete());
+                // Console.WriteLine("____________________________");
 
-                EternalGoal e1 = new EternalGoal("Eternal", "Eternal", 30);
-                e1.GetGoal();
-                e1.RecordEvent();
-                Console.WriteLine(e1.IsComplete());
-                Console.WriteLine("____________________________");
+                // EternalGoal e1 = new EternalGoal("Eternal", "Eternal", 30);
+                // e1.GetGoal();
+                // e1.RecordEvent();
+                // Console.WriteLine(e1.IsComplete());
+                // Console.WriteLine("____________________________");
 
-                ChecklistGoal c1 = new ChecklistGoal("ChecklistT", "ChecklistT", 40, true, 1, 3, 10);
-                c1.GetGoal();
-                c1.RecordEvent();
-                Console.WriteLine(c1.IsComplete());
-                Console.WriteLine("____________________________");
+                // ChecklistGoal c1 = new ChecklistGoal("ChecklistT", "ChecklistT", 40, true, 1, 3, 10);
+                // c1.GetGoal();
+                // c1.RecordEvent();
+                // Console.WriteLine(c1.IsComplete());
+                // Console.WriteLine("____________________________");
 
-                ChecklistGoal c2 = new ChecklistGoal("ChecklistF", "ChecklistF", 40, false, 3, 3, 10);
-                c2.GetGoal();
-                c2.RecordEvent();
-                Console.WriteLine(c2.IsComplete());
-                Console.WriteLine("____________________________");
+                // ChecklistGoal c2 = new ChecklistGoal("ChecklistF", "ChecklistF", 40, false, 3, 3, 10);
+                // c2.GetGoal();
+                // c2.RecordEvent();
+                // Console.WriteLine(c2.IsComplete());
+                // Console.WriteLine("____________________________");
                 
 
             }
