133eceb2a0e427dbeb5ac5e2c48bf43cf87a296b
stewarterin <erinlu@live.com>
2025-12-15 02:39:59 -0700

    Added method to Create a new goal and DisplayGoals method, fixed error in List variable

--- prove/Develop05/GoalTracker.cs
+++ prove/Develop05/GoalTracker.cs
@@ -2,14 +2,53 @@ using System.Runtime.CompilerServices;
 
 public class GoalTracker
 {
-    private List<string> _goals = new List<string>
-    {
-        
+    private List<Goal> _goals = new List<Goal>
+    {   
     };
 
+    public void CreateNewGoal()
+    {
+        Console.WriteLine("The types of goals are:");
+        Console.WriteLine("1. Simple Goal");
+        Console.WriteLine("2. Eternal Goal");
+        Console.WriteLine("3. Checklist Goal");
+        Console.Write("Which type of goal would you like to create? "); 
+        
+        int goalNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("───────────────────────────────");
+
+        Console.Write("What is the name of your goal? ");
+        string name = Console.ReadLine();
+        Console.Write("What is a short description of it? ");
+        string description = Console.ReadLine();
+        Console.Write("What is the amount of points associated with this goal? ");
+        int points = int.Parse(Console.ReadLine());
+        
+        if (goalNumber == 1)
+        {
+            _goals.Add(new SimpleGoal(name, description, points, false));
+        }
+        else if (goalNumber == 2)
+        {
+            _goals.Add(new EternalGoal(name, description, points));
+        }
+        else if (goalNumber == 3)
+        {
+            Console.Write("How many times does this goal need to be accomplished for a bonus? ");
+            int target = int.Parse(Console.ReadLine());
+            Console.Write("What is the bonus for accomplishing it that many times? ");
+            int bonus = int.Parse(Console.ReadLine());
+
+            _goals.Add(new ChecklistGoal(name, description, points, false, 0, target, bonus));
+        }
+    }
+    
     public void DisplayGoals()
     {
-        
+        foreach (Goal goal in _goals)
+        {
+            goal.GetGoal();
+        }
     }
 
     public void SaveGoals()
