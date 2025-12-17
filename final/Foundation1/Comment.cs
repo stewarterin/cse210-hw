bfb3926199c21bbe817c6f8860ae46eae9d3ce89
stewarterin <erinlu@live.com>
2025-12-17 01:27:36 -0700

    Added constructor, Added DisplayComment code

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -3,8 +3,13 @@ public class Comment
     public string _name;
     public string _text;
 
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
     public void DisplayComment()
     {
-        
+        Console.WriteLine($"-{_name} '{_text}'");
     }
 }
