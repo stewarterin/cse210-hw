d3b067cb0d53ea370f983ebad4d142f85a0d7449
stewarterin <erinlu@live.com>
2025-12-17 13:19:57 -0700

    Starting outline for the class

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,4 +1,19 @@
 public class Order
 {
-    
+    private List<Product> _products = new List<Product>();
+
+    public int GetCost()
+    {
+        return ; 
+    }
+
+    public void GetShipLabel()
+    {
+        
+    }
+
+    public void GetPackLabel()
+    {
+        
+    }
 }
