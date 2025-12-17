a53db829e1177f170f0990df3bbc82833759c783
stewarterin <erinlu@live.com>
2025-12-17 01:26:10 -0700

    Added constructor, Added DisplayVideoDetails code

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -10,13 +10,33 @@ public class Video
         
     };
 
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length; 
+    }
+
     public int GetCommentCount()
     {
         return _count;
     }
 
+    public void AddVideo()
+    {
+        
+    }
+
     public void DisplayVideoDetails()
     {
-        Console.WriteLine("");
+        Console.WriteLine($"Video: {_title} by {_author}");
+        Console.WriteLine($"Length: {_length} seconds");
+        Console.WriteLine($"Number of comments: {_count}");
+        Console.WriteLine($"Comments:");
+        
+        foreach (Comment comment in _comments)
+        {
+            comment.DisplayComment();
+        }
     }
 }
