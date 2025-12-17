07b2f3fe5c157a2a729e03bf5581b88bf9746881
stewarterin <erinlu@live.com>
2025-12-17 05:05:07 -0700

    Setting up outline of the classes

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,11 @@
+public class Customer
+{
+    private string _customerName;
+    private string _address;
+
+    public Customer(string name, string address)
+    {
+        _customerName = name;
+        _address = address;
+    }
+}
---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,8 @@
+public class Product
+{
+    private string _productName;
+    private int _id;
+    private int _pricePerUnit;
+    private int _quantity;
+
+}
