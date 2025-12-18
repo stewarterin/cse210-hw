8dca98312ec53b7fd5400075bfdb885907cc0ebb
stewarterin <erinlu@live.com>
2025-12-17 21:52:23 -0700

    Added customer variable, Added code for GetCost method

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,19 +1,42 @@
+using System.Numerics;
+
 public class Order
 {
     private List<Product> _products = new List<Product>();
+    private Customer _customer;
 
     public int GetCost()
     {
-        return ; 
+        int _finalCost = 0;
+        
+        foreach (Product product in _products)
+        {
+            product.GetTotalCost();
+        }
+
+        if (_customer.InUSA() == true)
+        {
+            _finalCost += 5;
+        }
+        else
+        {
+            _finalCost += 35;
+        }
+        return _finalCost; 
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
     }
 
     public void GetShipLabel()
     {
-        
+
     }
 
     public void GetPackLabel()
     {
-        
+
     }
 }
